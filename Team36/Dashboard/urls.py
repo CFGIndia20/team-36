17ace309e4ada908453abc0c7b3aadc87bdc0bd5
palak davda <palakdavda2202@gmail.com>
2020-07-26 00:13:03 +0530

    Report For Feedback to Donors

--- Team36/Dashboard/urls.py
+++ Team36/Dashboard/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('register/',views.registerPage,name="register"),
     path('login/',views.loginPage,name="login"),
     path('logout/',views.logoutUser,name="logout"),
+    path('msg/', views.load_twilio_config, name='index'),
 ]
